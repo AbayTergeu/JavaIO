40195bc90102801ea20563c2fbd1e4c43c924bce
Abay Tergeusizov <abaytergeu@gmail.com>
2016-10-24 11:27:22 +0600

    add

--- src/main/java/com/abter/App.java
+++ src/main/java/com/abter/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        Long currentUserId = 5L;
+        Integer userId = 5;
+        if(currentUserId.equals(userId.longValue())){
+            System.out.println( "Equals!" );
+        }
         System.out.println( "Hello World!" );
     }
 }
